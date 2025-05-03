4f0f912e0c15f8e6aa2ae12364390a94615d00e8
Michael D Labriola <veggiemike@sourceruckus.org>
2025-05-03 00:27:40 -0400

    drbd: imported sources from upstream 9.2.13
    % cd /scrap/drbd
    % make KDIR=/scrap/linux-mdl-6.12
    % cd /scrap/linux-mdl-6.12/drivers/block/drbd
    % ./import-upstream /scrap/drbd
    
    Copied Makefile from 6.6 kernel tree, which had already worked out all
    the required tweaks for the v9.2.13 upgrade.
    
    % cd ../../..
    % git add drivers/block/drbd include/linux

---
+++ drivers/block/drbd/drbd_buildtag.c
@@ -0,0 +1,11 @@
+/* automatically generated. DO NOT EDIT. */
+/* FIXME: not really, i wrote this by hand... ;-)
+ */
+
+#include <linux/drbd.h>
+#include <linux/drbd_config.h>
+
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: 0457237e0448663529fe161781873b356f17b3c5";
+}
---
+++ include/linux/drbd_config.h
@@ -0,0 +1,30 @@
+# 1 "/scrap/drbd/drbd/linux/drbd_config.h"
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+  drbd_config.h
+  DRBD's compile time configuration.
+*/
+
+#ifndef DRBD_CONFIG_H
+#define DRBD_CONFIG_H
+
+extern const char *drbd_buildtag(void);
+
+#define REL_VERSION "9.2.13"
+#define PRO_VERSION_MIN 118 /* 9.0.26 */
+#define PRO_VERSION_MAX 122
+
+#define PRO_VERSION_8_MIN 86
+#define PRO_VERSION_8_MAX 101
+
+/* We support two ranges of DRBD protocol version:
+ *  86-101: accepted DRBD 8 protocol versions as "rolling upgrade" path
+ * 102-109: never defined
+ * 110-117: _rejected_ because of bugs in the backward compat path
+ *	in more recent DRBD versions.  That is 9.0.0 to 9.0.25 inclusive.
+ *	"Rolling" upgrade path for those versions:
+ *	first upgrade to 9.0.latest, then connect to 9.1/9.2 or later.
+ * 118-PRO_VERSION_MAX: accepted DRBD 9 protocol versions.
+ */
+
+#endif
